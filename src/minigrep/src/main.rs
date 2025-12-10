405461e8676f9780744621e2790ee33da76f2838
Marcos F <84358344+themarcosf@users.noreply.github.com>
2025-12-10 15:47:15 -0300

    .

--- src/minigrep/src/main.rs
+++ src/minigrep/src/main.rs
@@ -49,12 +49,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {}", err);
+        eprintln!("Error parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
